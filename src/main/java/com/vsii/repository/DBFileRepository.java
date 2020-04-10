c1a40fc4ae9f512a13173bdf035044b1dae98cf8
Phi Nam <phinamhd1992@gmail.com>
2020-04-10 10:11:24 +0700

    create upload file

---
+++ src/main/java/com/vsii/repository/DBFileRepository.java
@@ -0,0 +1,10 @@
+package com.vsii.repository;
+
+import com.vsii.model.DBFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DBFileRepository extends JpaRepository<DBFile, String> {
+
+}
